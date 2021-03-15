d6f49679941687ce56910dce548aeef45d5b0b4f
Elijah Atkins <elijahorama@gmail.com>
2021-03-15 14:37:04 -0600

    moved eye styles to eye component

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 
 const Eyes = () => {
+  const animationSeconds = 7
+  const eyeStyle = {
+    width: "250px",
+    height: "250px",
+    backgroundSize: "contain",
+    backgroundPosition: "50% 50%",
+    backgroundRepeat: "no-repeat",
+    animation: `blink ${animationSeconds}s infinite`,
+    animationTimingFunction: "steps(1, end)"
+  }
   return (
-    <div className="eyes">
-      <div className="eye"></div>
-      <div className="eye"></div>
+    <div className="eyes" style={{
+      display: "flex",
+      justifyContent: "space-around"
+    }}>
+      <div className="eye" style={eyeStyle}></div>
+      <div className="eye" style={eyeStyle}></div>
     </div>
   );
 };
